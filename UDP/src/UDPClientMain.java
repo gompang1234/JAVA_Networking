dca887dc0aaa72dcaeacdc3ab3a2e6699fbb2c39
Mirim <mirim@e-mirim.hs.kr>
2015-09-14 17:13:44 +0900

    2015 09 14
    add UPDServer  address>message

--- UDP/src/UDPClientMain.java
+++ UDP/src/UDPClientMain.java
@@ -1,19 +1,26 @@
+import java.io.IOException;
+import java.net.*;
 import java.util.Scanner;
 
 /**
  * Created by Mirim on 2015-09-10.
  */
 public class UDPClientMain {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         String message="";
         Scanner sc=new Scanner(System.in);
         //input message
         System.out.println(" 보낼 메시지 > ");
         message=sc.nextLine();
         //make packet
-
+        DatagramPacket dp = new DatagramPacket(
+                message.getBytes(),
+                message.getBytes().length,
+                InetAddress.getByName("localhost"),   //보낼 주소
+                1226);  //보낼포트
         //make socket
-
+        DatagramSocket ds = new DatagramSocket();
         //send
+        ds.send(dp);
     }
 }
