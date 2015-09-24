7109f89e751b1f30cda042c42184f5d057b1770a
Mirim <mirim@e-mirim.hs.kr>
2015-09-24 17:24:28 +0900

    2015 09 24
    final commit
    add URLMain+GUI

--- URL/src/URLMain.java
+++ URL/src/URLMain.java
@@ -1,5 +1,11 @@
+import java.awt.*;
 import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by Mirim on 2015-09-24.
@@ -11,10 +17,37 @@ public class URLMain {
         String line ="";
         StringBuffer sb = new StringBuffer();
         //URL
+        //bloter:http://feeds.feedburner.com/bloter
+        String urlString = "http://kr.hani.feedsportal.com/c/34762/f/640633/index.rss";
         //make Reader
-        //read
-        //print
+        try {
+            url=new URL(urlString);
+            br= new BufferedReader(new InputStreamReader(url.openStream(),"UTF-8"));
+            //read
+            while((line = br.readLine()) !=null){
+                //print
+                sb.append(line);
+               // System.out.println(line);
+            }
+        } catch (MalformedURLException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         //make GUI
+        Frame frame = new Frame("Bloter");
+        List list = new List();
+        frame.add(list);
+
         //pattern matching
+        Pattern pattern = Pattern.compile("<title>(.+?)</title>");
+        Matcher matcher = pattern.matcher(sb.toString());
+        while(matcher.find()){
+        //    System.out.println(matcher.group(1).trim());
+            list.add(matcher.group(1).trim());
+        }
+
+        frame.setSize(400,400);
+        frame.setVisible(true);
     }
 }
